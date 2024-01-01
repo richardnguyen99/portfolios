65aa494556329a4c6b33a771997a6bf7fb218da7
Richard Nguyen <richard@richardhnguyen.com>
2023-12-31 21:54:46 -0800

    feat: add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import * as React from "react";
 
+import "./index.css";
+
 const App: React.FC = () => {
   return (
     <>
-      <div>Hello, World</div>
+      <div className="text-3xl font-bold underline">Hello, World</div>
     </>
   );
 };
