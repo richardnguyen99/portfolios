abea9614a5dd5d26bc0c618acfd1056853c144fd
Richard Nguyen <richard@richardhnguyen.com>
2024-01-17 21:56:36 -0800

    fix: select modal instead of adding when existing

--- src/contexts/Modal/Provider.tsx
+++ src/contexts/Modal/Provider.tsx
@@ -7,24 +7,6 @@ import { Window } from "@components";
 const ModalProvider: React.FC<ModalProviderProps> = ({ children }) => {
   const [modals, setModals] = React.useState<ModalProps[]>([]);
 
-  const addModal = React.useCallback(
-    (modal: ModalProps) => {
-      const filteredModal = modals.map((modal) => ({
-        ...modal,
-        active: false,
-      }));
-
-      setModals(() => [...filteredModal, modal]);
-    },
-    [modals],
-  );
-
-  const closeModal = React.useCallback((id: string) => {
-    setModals((prev) => prev.filter((modal) => modal.id !== id));
-  }, []);
-
-  const closeAllModals = React.useCallback(() => {}, []);
-
   const selectModal = React.useCallback(
     (id: string) => {
       const targetModal = modals.filter((modal) => modal.id === id)[0];
@@ -42,6 +24,33 @@ const ModalProvider: React.FC<ModalProviderProps> = ({ children }) => {
     },
     [modals],
   );
+
+  const addModal = React.useCallback(
+    (modal: ModalProps) => {
+      const existingModal = modals.filter(
+        (existingModal) => modal.id === existingModal.id,
+      );
+
+      if (existingModal.length > 0) {
+        selectModal(modal.id);
+        return;
+      }
+
+      const filteredModal = modals.map((modal) => ({
+        ...modal,
+        active: false,
+      }));
+
+      setModals(() => [...filteredModal, modal]);
+    },
+    [modals, selectModal],
+  );
+
+  const closeModal = React.useCallback((id: string) => {
+    setModals((prev) => prev.filter((modal) => modal.id !== id));
+  }, []);
+
+  const closeAllModals = React.useCallback(() => {}, []);
 
   const deselectAllModals = React.useCallback(() => {
     setModals((prev) =>
