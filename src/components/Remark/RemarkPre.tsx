f742ab4ccfb09ecf154c094a863f7672af1531e9
Richard Nguyen <richard@richardhnguyen.com>
2024-01-18 16:17:26 -0800

    fix: add break white spaces on pre elements

--- src/components/Remark/RemarkPre.tsx
+++ src/components/Remark/RemarkPre.tsx
@@ -11,6 +11,7 @@ const RemarkPre: React.FC<RemarkPreProps> = ({ children, ...rest }) => {
         "rounded-lg text-sm border",
         "mb-6",
         "px-6 py-4",
+        "whitespace-break-spaces",
         "dark:border-slate-600 border-slate-400",
         "dark:bg-slate-900 bg-gray-200",
       )}
