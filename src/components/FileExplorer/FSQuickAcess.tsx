9693c4ed72d7bdffd7e21189d9941d2eeac74c09
Richard Nguyen <richard@richardhnguyen.com>
2024-02-05 16:21:24 -0800

    feat: add style for FS quick access items

--- src/components/FileExplorer/FSQuickAcess.tsx
+++ src/components/FileExplorer/FSQuickAcess.tsx
@@ -1,5 +1,53 @@
 import * as React from "react";
 import clsx from "classnames";
+import {
+  ClockIcon,
+  FileIcon,
+  HomeIcon,
+  MoveToBottomIcon,
+  StarIcon,
+} from "@primer/octicons-react";
+import {
+  ComputerDesktopIcon,
+  MusicalNoteIcon,
+  VideoCameraIcon,
+} from "@heroicons/react/16/solid";
+
+const FSQuickAccessItem: React.FC<React.PropsWithChildren> = ({ children }) => {
+  return (
+    <div
+      className={clsx(
+        "flex items-center gap-2",
+        "text-base font-bold",
+        "px-2 py-1 rounded-md",
+        "hover:bg-gray-200 dark:hover:bg-gray-600",
+        "active:bg-gray-300/40 dark:active:bg-gray-600/60",
+        "transition-colors duration-50 ease-in-out",
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+const FSQuickAccessSubItem: React.FC<React.PropsWithChildren> = ({
+  children,
+}) => {
+  return (
+    <div
+      className={clsx(
+        "flex items-center gap-1",
+        "text-sm",
+        "px-1 py-1 rounded-md",
+        "hover:bg-gray-200 dark:hover:bg-gray-600",
+        "active:bg-gray-300/40 dark:active:bg-gray-600/60",
+        "transition-colors duration-50 ease-in-out",
+      )}
+    >
+      {children}
+    </div>
+  );
+};
 
 const FSQuickAccess: React.FC = () => {
   return (
@@ -16,15 +64,55 @@ const FSQuickAccess: React.FC = () => {
       <h1 className="text-lg font-extrabold">Quick Access</h1>
       <div className="flex flex-col gap-3 mt-3">
         <div className="flex flex-col gap-1">
-          <h2 className="text-base font-bold">Recent</h2>
-          <h2 className="text-base font-bold">Starred</h2>
-          <h2 className="text-base font-bold">Home</h2>
-          <div className="flex flex-col gap-1 ml-3">
-            <h3 className="text-sm">Documents</h3>
-            <h3 className="text-sm">Downloads</h3>
-            <h3 className="text-sm">Music</h3>
-            <h3 className="text-sm">Pictures</h3>
-            <h3 className="text-sm">Videos</h3>
+          <FSQuickAccessItem>
+            <>
+              <ClockIcon />
+              <span>Recent</span>
+            </>
+          </FSQuickAccessItem>
+          <FSQuickAccessItem>
+            <>
+              <StarIcon />
+              <span>Starred</span>
+            </>
+          </FSQuickAccessItem>
+          <FSQuickAccessItem>
+            <>
+              <HomeIcon />
+              <span>Home</span>
+            </>
+          </FSQuickAccessItem>
+          <div className="flex flex-col gap-2 ml-[26px] mt-1">
+            <FSQuickAccessSubItem>
+              <>
+                <FileIcon />
+                <span>Documents</span>
+              </>
+            </FSQuickAccessSubItem>
+            <FSQuickAccessSubItem>
+              <>
+                <MoveToBottomIcon />
+                <span>Downloads</span>
+              </>
+            </FSQuickAccessSubItem>
+            <FSQuickAccessSubItem>
+              <>
+                <MusicalNoteIcon className="w-4 h-4" />
+                <span>Music</span>
+              </>
+            </FSQuickAccessSubItem>
+            <FSQuickAccessSubItem>
+              <>
+                <VideoCameraIcon className="w-4 h-4" />
+                <span>Videos</span>
+              </>
+            </FSQuickAccessSubItem>
+            <FSQuickAccessSubItem>
+              <>
+                <ComputerDesktopIcon className="w-4 h-4" />
+                <span>Desktop</span>
+              </>
+            </FSQuickAccessSubItem>
           </div>
         </div>
       </div>
