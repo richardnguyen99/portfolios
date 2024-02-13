2ce09d4b28e0e742c00b50208dc04b303f4f4453
Richard Nguyen <richard@richardhnguyen.com>
2024-02-12 23:27:09 -0800

    feat: add file explorer dialog components

---
+++ src/components/FileExplorer/Dialog/AddNewFile.tsx
@@ -0,0 +1,46 @@
+import * as React from "react";
+import clsx from "classnames";
+import * as AlertDialog from "@radix-ui/react-alert-dialog";
+
+type Props = AlertDialog.AlertDialogContentProps & {
+  onCanceled?: () => void;
+  onSaved?: () => void;
+};
+
+const AddNewFileDialogRenderer: React.ForwardRefRenderFunction<
+  HTMLDivElement,
+  Props
+> = (props, ref) => {
+  const { onCanceled, onSaved, ...rest } = props;
+
+  return (
+    <AlertDialog.Content
+      {...rest}
+      ref={ref}
+      forceMount
+      className={clsx(
+        "fixed top-1/2 left-1/2 w-96",
+        "transform -translate-x-1/2 -translate-y-1/2",
+        "rounded-md p-4 z-[1000]",
+        "bg-white dark:bg-gray-800",
+      )}
+    >
+      <AlertDialog.Description className="mb-4">
+        Are you sure you want to delete this file?
+      </AlertDialog.Description>
+      <div>
+        <AlertDialog.Cancel asChild onClick={onCanceled}>
+          <button className="btn">Cancel</button>
+        </AlertDialog.Cancel>
+        <AlertDialog.Action asChild onClick={onSaved}>
+          <button className="btn btn-red">Delete</button>
+        </AlertDialog.Action>
+      </div>
+    </AlertDialog.Content>
+  );
+};
+
+const AddNewFileDialog = React.forwardRef(AddNewFileDialogRenderer);
+AddNewFileDialog.displayName = "AddNewFileDialog";
+
+export default AddNewFileDialog;
--- src/components/FileExplorer/FSView.tsx
+++ src/components/FileExplorer/FSView.tsx
@@ -12,17 +12,21 @@ import { ModalProps } from "@contexts/Modal/type";
 import { Terminal } from "@components";
 import useFileExplorer from "./hook";
 import useModal from "@contexts/Modal/useModal";
-import useSystemCall from "@contexts/SystemCall/useSystemCall";
 import useWindow from "@components/Window/useWindow";
+import AddNewFileDialog from "./Dialog/AddNewFile";
+import AddNewFolderDialog from "./Dialog/AddNewFolder";
 
 const FSView: React.FC = () => {
   const { currDir } = useFileExplorer();
   const { addModal } = useModal();
-  const { addFile, addDirectory } = useSystemCall();
   const { getId } = useWindow();
 
   const windowRef = React.useRef<HTMLElement | null>(null);
-  const [dialogOpen, setDialogOpen] = React.useState(false);
+  const [dialog, setDialog] = React.useState({
+    open: false,
+    dialog: null as React.FC | null,
+    props: null as object | null,
+  });
 
   const handleOpenTerminalClick = React.useCallback(() => {
     const newTerminal: ModalProps = {
@@ -41,9 +45,47 @@ const FSView: React.FC = () => {
     addModal(newTerminal);
   }, [addModal, currDir]);
 
-  const handleNewFolderClick = React.useCallback((e: React.MouseEvent) => {
-    setDialogOpen(true);
+  const handleCloseDialog = React.useCallback(() => {
+    setDialog({
+      open: false,
+      dialog: null,
+      props: null,
+    });
   }, []);
+
+  const handleNewFolderClick = React.useCallback(() => {
+    setDialog({
+      open: true,
+      dialog: AddNewFolderDialog,
+      props: {
+        onCanceled: () => {
+          console.log("new folder canceled");
+          handleCloseDialog();
+        },
+        onSaved: () => {
+          console.log("new folder saved");
+          handleCloseDialog();
+        },
+      },
+    });
+  }, [handleCloseDialog]);
+
+  const handleNewFileClick = React.useCallback(() => {
+    setDialog({
+      open: true,
+      dialog: AddNewFileDialog,
+      props: {
+        onCanceled: () => {
+          console.log("new file canceled");
+          handleCloseDialog();
+        },
+        onSaved: () => {
+          console.log("new file saved");
+          handleCloseDialog();
+        },
+      },
+    });
+  }, [handleCloseDialog]);
 
   React.useEffect(() => {
     const windowContainer = document.querySelector(
@@ -53,8 +95,10 @@ const FSView: React.FC = () => {
     windowRef.current = windowContainer;
   }, [getId]);
 
+  const Dialog = dialog.dialog;
+
   return (
-    <AlertDialog.Root open={dialogOpen}>
+    <AlertDialog.Root open={dialog.open}>
       <ContextMenuPrimitive.Root>
         <ContextMenuPrimitive.Trigger className="relative flex flex-col w-full justify-between select-none overflow-x-hidden">
           <DragSelect.Provider initialSettings={{ selectedClass: "selector" }}>
@@ -87,6 +131,23 @@ const FSView: React.FC = () => {
             <div className="flex items-center gap-2">
               <div className="w-4 h-4"></div>
               <div>New Folder</div>
+            </div>
+            <div className="ml-auto font-mono font-light text-xs">
+              something
+            </div>
+          </ContextMenuPrimitive.Item>
+          <ContextMenuPrimitive.Item
+            onClick={handleNewFileClick}
+            className={clsx(
+              "flex items-center",
+              "px-3 py-2 rounded-md",
+              "hover:bg-gray-400 dark:hover:bg-gray-600",
+              "outline-none focus:outline-none border-none",
+            )}
+          >
+            <div className="flex items-center gap-2">
+              <div className="w-4 h-4"></div>
+              <div>New File</div>
             </div>
             <div className="ml-auto font-mono font-light text-xs">
               something
@@ -126,44 +187,26 @@ const FSView: React.FC = () => {
       </ContextMenuPrimitive.Root>
       {windowRef.current ? (
         <AlertDialog.Portal container={windowRef.current} forceMount>
-          <Transition.Root show={dialogOpen}>
+          <Transition.Root show={dialog.open}>
             <Transition.Child as={React.Fragment} leave="delay-100">
               <AlertDialog.Overlay
                 className="fixed inset-0 bg-black bg-opacity-50 z-[999]"
                 forceMount
               />
             </Transition.Child>
-            <Transition.Child
-              as={React.Fragment}
-              enter="transition ease-out duration-100 delay-0"
-              enterFrom="transform opacity-0 scale-95"
-              enterTo="transform opacity-100 scale-100"
-              leave="transition ease-in duration-75"
-              leaveFrom="transform opacity-100 scale-100"
-              leaveTo="transform opacity-0 scale-95"
-            >
-              <AlertDialog.Content
-                className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-96 bg-white dark:bg-gray-800 rounded-md p-4 z-[1000]"
-                forceMount
+            {Dialog && (
+              <Transition.Child
+                as={React.Fragment}
+                enter="transition ease-out duration-100 delay-0"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
               >
-                <AlertDialog.Description className="mb-4">
-                  Are you sure you want to delete this file?
-                </AlertDialog.Description>
-                <div>
-                  <AlertDialog.Cancel
-                    asChild
-                    onClick={() => {
-                      setDialogOpen(false);
-                    }}
-                  >
-                    <button className="btn">Cancel</button>
-                  </AlertDialog.Cancel>
-                  <AlertDialog.Action asChild>
-                    <button className="btn btn-red">Delete</button>
-                  </AlertDialog.Action>
-                </div>
-              </AlertDialog.Content>
-            </Transition.Child>
+                <Dialog {...dialog.props} />
+              </Transition.Child>
+            )}
           </Transition.Root>
         </AlertDialog.Portal>
       ) : null}
